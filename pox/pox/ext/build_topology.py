9f4f0c055e5ffb22ea27cad6a2d41edfcd0a19e4
Chris Collins <chris.collins.1210@gmail.com>
2018-05-04 23:58:59 +0000

    got it working

--- pox/pox/ext/build_topology.py
+++ pox/pox/ext/build_topology.py
@@ -23,7 +23,7 @@ n_nbr_switches_per_switch = 3
 
 class JellyFishTop(Topo):
     def build(self):
-        self._switches = [self.addSwitch('s%d' % x) for x in range(n_switches)]
+        self._switches = [self.addHost('s%d' % x) for x in range(n_switches)]
         for i, switch in enumerate(self._switches):
             for x in range(n_hosts_per_switch):
                 host = self.addHost('h%ds%d' % (x, i))
@@ -48,11 +48,17 @@ def experiment(net):
 def main():
     topo = JellyFishTop()
 
-    net = Mininet(topo=topo, host=CPULimitedHost, link = TCLink)
-    for i, sid in enumerate(topo._switches):
-        switch = net.get(sid)
-        for j, intf in enumerate(switch.intfList()):
-            intf.setIP("10.1.%d.%d/32" % (i, j))
+    net = Mininet(topo=topo, host=CPULimitedHost, link = TCLink, controller = JELLYPOX)
+    for i, edge in enumerate(topo._graph.edges()):
+        src_sid = edge[0]
+        dst_sid = edge[1]
+        src = net.get('s%d' % src_sid)
+        dst = net.get('s%d' % dst_sid)
+        dst_iface_on_src, src_iface_on_dst = src.connectionsTo(dst)[0]
+        dst_iface_on_src.setIP("10.1.%d.4/24" % i)
+        src_iface_on_dst.setIP("10.1.%d.7/24" % i)
+        src.setARP(dst.IP(src_iface_on_dst), dst.MAC(src_iface_on_dst))
+        dst.setARP(src.IP(dst_iface_on_src), dst.MAC(dst_iface_on_src))
 
     for i, sid in enumerate(topo._switches):
         switch = net.get(sid)
@@ -69,26 +75,17 @@ def main():
                 nh_sid = 's%d' % path[1]
                 nh_switch = net.get(nh_sid)
                 nh_if, reverse = switch.connectionsTo(nh_switch)[0]
-                print "%s <=> %s, %s <=> %s" % (sid, nh_sid, nh_if, reverse)
+                print "%s <=> %s, %s <=> %s" % (sid, nh_sid, nh_if.IP(), reverse.IP())
                 for intf in nh_switch.intfList():
                     print intf.IP()
                 nh_ip = nh_switch.IP(reverse)
                 for x in range(n_hosts_per_switch):
                     hid = 'h%ds%d' % (x, j)
                     host_ip = net.get(hid).IP()
-                    cmd = "route add -net %s netmask 255.255.255.255 gw %s dev %s" % (host_ip, nh_ip, nh_if)
+                    cmd = "route add -host %s gw %s dev %s" % (host_ip, nh_ip, nh_if)
                     print cmd
                     switch.sendCmd(cmd)
-                    switch.waitOutput()
-    for edge in topo._graph.edges():
-        src_sid = 's%d' % edge[0]
-        dst_sid = 's%d' % edge[1]
-        src = net.get(src_sid)
-        dst = net.get(dst_sid)
-        dst_iface_on_src, src_iface_on_dst = src.connectionsTo(dst)[0]
-        src.setARP(dst.IP(src_iface_on_dst), dst.MAC(src_iface_on_dst))
-        dst.setARP(src.IP(dst_iface_on_src), dst.MAC(dst_iface_on_src))
-        #src.sendCmd("arp -i %s" % dst_iface_on_src)
+                    print switch.waitOutput()
     experiment(net)
 if __name__ == "__main__":
     main()
