688e6bc57631d9927016f0f80f83ddc894f077c7
Chris Collins <chris.collins.1210@gmail.com>
2018-05-06 23:49:31 +0000

    smarter link addrs

--- pox/pox/ext/build_topology.py
+++ pox/pox/ext/build_topology.py
@@ -19,7 +19,7 @@ from k_shortest_paths import *
 
 n_switches = 60
 n_hosts_per_switch = 2
-n_nbr_switches_per_switch = 5
+n_nbr_switches_per_switch = 4
 
 class JellyFishTop(Topo):
     def build(self):
@@ -76,7 +76,7 @@ def smart_pingall(net, topo):
 def experiment(net, topo):
     net.start()
     sleep(1)
-    # smart_pingall(net, topo)
+    #smart_pingall(net, topo)
     CLI(net)
     # net.pingAll()
     net.stop()
@@ -93,7 +93,7 @@ def main():
         switch.sendCmd("ip route flush table main")
         o(switch.waitOutput())
         switch.sendCmd("sysctl -w net.ipv4.ip_forward=1")
-        o(switch.waitOutput())
+        switch.waitOutput()
     print "tables flushed and forwarding turned on"
 
     for i, edge in enumerate(topo._graph.edges()):
@@ -102,9 +102,8 @@ def main():
         src = net.get('s%d' % src_sid)
         dst = net.get('s%d' % dst_sid)
         dst_iface_on_src, src_iface_on_dst = src.connectionsTo(dst)[0]
-        split_addr = (i // 255, i % 255)
-        dst_iface_on_src.setIP("11.%d.%d.4/24" % split_addr)
-        src_iface_on_dst.setIP("11.%d.%d.7/24" % split_addr)
+        dst_iface_on_src.setIP("11.%d.%d.4/24" % (src_sid, dst_sid))
+        src_iface_on_dst.setIP("11.%d.%d.7/24" % (src_sid, dst_sid))
         src.setARP(dst.IP(src_iface_on_dst), dst.MAC(src_iface_on_dst))
         dst.setARP(src.IP(dst_iface_on_src), dst.MAC(dst_iface_on_src))
 
@@ -120,7 +119,7 @@ def main():
             switch_iface_on_host.setIP("10.2.%d.%d/31" % (i, 2 * x + 3))
             host.setARP(switch.IP(host_iface_on_switch), switch.MAC(host_iface_on_switch))
             switch.setARP(host.IP(switch_iface_on_host), host.MAC(switch_iface_on_host))
-            host.sendCmd("route add -net 10.0.0.0 netmask 255.0.0.0 gw %s dev %s" % (host_iface_on_switch.IP(), switch_iface_on_host))
+            host.sendCmd("route add -net 0.0.0.0 netmask 0.0.0.0 gw %s dev %s" % (host_iface_on_switch.IP(), switch_iface_on_host))
             o(host.waitOutput())
 
     print "switch/switch links configured"
